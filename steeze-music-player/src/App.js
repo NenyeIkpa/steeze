4becfe9c65cef96c8ac938fab5a9272c296a8d90
NenyeIkpa <nenyeikpa@gmail.com>
2024-07-10 23:44:34 +0100

    implemented auth with google

--- steeze-music-player/src/App.js
+++ steeze-music-player/src/App.js
@@ -1,15 +1,47 @@
-import './App.css';
-import Songs from './Songs';
-import PlayList from './Playlist';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Login, Home } from './components';
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { app } from './config/firebase.config'
 
-function App() {
+const App = () => {
+
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate()
+  const [auth, setAuth] = useState(false || window.localStorage.getItem('auth') === "true")
+
+  
+useEffect(() => {
+  firebaseAuth.onAuthStateChanged((userCred) => {
+    if (userCred) {
+      userCred.getIdToken().then((token) => {
+      })
+    } else {
+      setAuth(false)
+      window.localStorage.setItem('auth', 'false');
+      navigate('/login')
+    }
+  })
+})
+
   return (
-    <div className="container">
-      <h1>Steeze Music Player</h1>
-      <PlayList />
-      <Songs />
+    <div className='w-screen h-screen bg-blue-400 flex justify-center items-center'>
+      <Routes>
+         <Route path='/login'element={<Login setAuth={setAuth} />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
     </div>
-  );
+  )
 }
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <h1>Steeze Music Player</h1>
+//       <PlayLists />
+//       <Songs />
+//     </div>
+//   );
+// }
 
 export default App;
